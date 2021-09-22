20bc698b32a5011b6c532ad5d7ded3e750c48a3e
hardikmittal1 <90177013+hardikmittal1@users.noreply.github.com>
2021-09-22 15:15:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,29 +1,14 @@
-const express = require('express');
-const app = express();
 
-app.get('/', (req, res) => {
-  console.log('Hello world received a request.');
+var express = require('express');
 
-  const target = process.env.TARGET || 'World';
-  res.send(`Hello ${target}!`);
+// Constants
+var PORT = 8080;
+
+// App
+var app = express();
+app.get('/', function (req, res) {
+    res.send('Hello world\n');
 });
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log('Hello world listening on port', port);
-});
-
-
-// var express = require('express');
-
-// // Constants
-// var PORT = 8080;
-
-// // App
-// var app = express();
-// app.get('/', function (req, res) {
-//     res.send('Hello world\n');
-// });
-
-// app.listen(PORT);
-// console.log('Running on ' + PORT);
+app.listen(PORT);
+console.log('Running on ' + PORT);
